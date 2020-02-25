fe0813ebecc2822282f10c846ae79007d98b2df3
jiro <ec2-user@ip-172-31-42-225.ec2.internal>
2020-02-25 02:15:03 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root to: "tasks#index"
-  resources :tasks
+  
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  post "login", to: "tasks#index"
+  
+  resources :tasks, only: [:index, :show, :new, :create]
+
 end
